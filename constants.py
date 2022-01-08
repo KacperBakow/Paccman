bbb9c3753b1aa42940017ba5c87383fb86d6e751
KacperBakow <97342745+KacperBakow@users.noreply.github.com>
2022-01-08 13:06:53 +0100

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,14 @@
+from pgzero.builtins import Actor, Rect
+
+HUD = 80
+GOLD = 255, 215, 0
+GRAY = 100, 100, 100
+RED = 255, 0, 0
+f_name = 'bungee-regular'
+
+
+def rect_for_pacman(pacman):
+    x, y, width, height = pacman.x, pacman.y, pacman.width, pacman.height
+    rect = Rect(x - width / 2, y - height / 2, width, height)
+    return rect
+
